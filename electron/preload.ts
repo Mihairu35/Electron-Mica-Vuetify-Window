ca0521f3be6e1b08e62d82ea5aa76f2738d19d9b
Mihairu <mis507@yandex.ru>
2023-03-20 01:53:38 +0300

    Removed extra body theme class

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,18 +1,2 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-const remote = require("@electron/remote");
-
-function updateTheme() {
-	let themeClass = remote.nativeTheme.shouldUseDarkColors
-		? "v-theme--ThemeDark"
-		: "v-theme--ThemeLight";
-	document.body.className = themeClass;
-}
-
-remote.getCurrentWindow().on("focus", function () {
-	updateTheme();
-});
-
-window.addEventListener("DOMContentLoaded", () => {
-	updateTheme();
-});
--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,19 @@
 <script>
 import WindowBar from "./components/core/WindowBar.vue";
 import Main from "./components/core/Main.vue";
+import { useTheme } from "vuetify";
+const remote = require("@electron/remote");
 
 export default {
+	setup() {
+		//vue theme change
+		const theme = useTheme();
+		remote.getCurrentWindow().on("focus", function () {
+			theme.global.name.value = remote.nativeTheme.shouldUseDarkColors
+				? "ThemeDark"
+				: "ThemeLight";
+		});
+	},
 	components: {
 		WindowBar,
 		Main,
